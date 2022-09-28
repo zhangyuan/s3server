c96354a451e80c19aa3db4483d7422e1129e083a
Yuan Zhang <zhangyuan@users.noreply.github.com>
2022-09-28 23:37:42 +0800

    serve s3 files

--- go.mod
+++ go.mod
@@ -3,8 +3,29 @@ module s3server
 go 1.19
 
 require (
+	github.com/aws/aws-sdk-go-v2/config v1.17.7
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.27.11
+	github.com/gin-gonic/gin v1.8.1
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.16.16 // indirect
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.4.8 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.12.20 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.17 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.23 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.17 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.24 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.0.14 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.9 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.18 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.17 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.13.17 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.11.23 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.13.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.16.19 // indirect
+	github.com/aws/smithy-go v1.13.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.8.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.1 // indirect
--- main.go
+++ main.go
@@ -1,17 +1,101 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"io"
 	"log"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	if err := invoke(); err != nil {
-		log.Fatalln(err)
+	if len(os.Args) != 2 {
+		log.Fatalf("invalid arguments.")
+	}
+
+	bucket := os.Args[1]
+
+	if err := serve(bucket); err != nil {
+		log.Fatalf("error occurs, %v", err)
 	}
 }
 
-func invoke() error {
-	fmt.Println("hello world")
+func serve(bucket string) error {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+
+	if err != nil {
+		return err
+	}
+
+	svc := s3.NewFromConfig(cfg)
+
+	r := gin.Default()
+
+	r.Use(func(ctx *gin.Context) {
+		output, err := GetObjectByPath(svc, bucket, ctx.Request.URL.Path)
+
+		if err != nil {
+			var nsk *types.NoSuchKey
+
+			if errors.As(err, &nsk) {
+				ctx.AbortWithStatus(404)
+			} else {
+				ctx.AbortWithStatus(500)
+			}
+			return
+		}
+
+		b := make([]byte, 128)
+		reader := output.Body
+		writer := ctx.Writer
+
+		for {
+			n, err := reader.Read(b)
+			writer.Write(b[:n])
+
+			if err == io.EOF {
+				writer.Flush()
+				break
+			}
+		}
+
+		ctx.AbortWithStatus(200)
+	})
+
+	r.Run(":5050")
 	return nil
+}
+
+func GetObjectByPath(svc *s3.Client, bucket string, path string) (*s3.GetObjectOutput, error) {
+	path = strings.TrimLeft(path, "/")
+
+	if path == "" || strings.HasSuffix(path, "/") {
+		path = fmt.Sprintf("%sindex.html", strings.TrimLeft(path, "/"))
+		return GetObject(svc, bucket, path)
+	}
+
+	output, err := GetObject(svc, bucket, path)
+
+	var nsk *types.NoSuchKey
+
+	if errors.As(err, &nsk) {
+		path = fmt.Sprintf("%s/index.html", path)
+		return GetObject(svc, bucket, path)
+	} else {
+		return output, err
+	}
+}
+
+func GetObject(svc *s3.Client, bucket string, key string) (*s3.GetObjectOutput, error) {
+	return svc.GetObject(context.TODO(), &s3.GetObjectInput{
+		Bucket: &bucket,
+		Key:    &key,
+	})
 }
